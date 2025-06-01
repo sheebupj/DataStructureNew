d917c830cd02500a3634cd6c860063b0dc82bf0e
Sheebu PJ <sheebupj@gmail.com>
2025-06-01 14:08:41 +0530

    test cases added for Insert,search,findMin ab=nd findMax

--- src/test/java/com/paremal/sheebu/data_structure/tree/BinaryTreeTest.java
+++ src/test/java/com/paremal/sheebu/data_structure/tree/BinaryTreeTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BinaryTreeTest {
     BinaryTree tree;
 
     @Before
-    public void setup(){
-        tree=new BinaryTree();
+    public void setup() {
+        tree = new BinaryTree();
         tree.insert(50);
         tree.insert(30);
         tree.insert(20);
@@ -24,8 +24,36 @@ public class BinaryTreeTest {
     }
 
     @Test
-    public void testCollect(){
-        List<Integer> clist=tree.collect();
-        assertEquals(Arrays.asList(20,30,40,50,60,70,80),clist);
+    public void testCollect() {
+        List<Integer> clist = tree.collect();
+        assertEquals(Arrays.asList(20, 30, 40, 50, 60, 70, 80), clist);
+    }
+
+    @Test
+    public void testInsert() {
+        assertFalse(tree.search(33));
+        tree.insert(33);
+        assertTrue(tree.search(33));
+
+    }
+    @Test
+    public void testSerch() {
+        assertFalse(tree.search(33));
+        tree.insert(33);
+        assertTrue(tree.search(33));
+
+    }
+    @Test
+    public void testFindMin() {
+        assertEquals(20,tree.findMin());
+        tree.insert(10);
+        assertEquals(10,tree.findMin());
+    }
+    @Test
+    public void testFindMax() {
+        assertEquals(80,tree.findMax());
+        tree.insert(100);
+        assertEquals(100,tree.findMax());
+
     }
 }
