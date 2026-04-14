92bb85c43f13b7c8b84e1c3ec8d7326fdf28344b
Sheebu P J <Sheebupj@gmail.com>
2026-04-14 18:54:05 +0530

    code modified

--- src/main/java/com/paremal/sheebu/algorithms/CheckOpeningClosingBracketsBalanced.java
+++ src/main/java/com/paremal/sheebu/algorithms/CheckOpeningClosingBracketsBalanced.java
@@ -4,15 +4,21 @@ package com.paremal.sheebu.algorithms;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
+/**
+ * This class provides functionality to check if the opening and closing brackets in a string are balanced.
+ */
 public class CheckOpeningClosingBracketsBalanced {
+    private static final Set<Character> OPENING_SET = Set.of('(', '{', '[');
+    private static final Set<Character> CLOSING_SET = Set.of(')', '}', ']');
+    private static final Map<Character, Character> OPENING_CLOSING = Map.of('(', ')', '{', '}', '[', ']');
 
 
+
+    /**
+     * Main method to read input from the user and check if brackets are balanced for each input line.
+     */
     public static void main(String[] argh) {
 
 
@@ -39,48 +45,44 @@ public class CheckOpeningClosingBracketsBalanced {
 
     }
 
-    /*
-    Method for checking parenthesis are balanced in a String
-
-    Iterate through String for each letter
-    when closing encountered then pop previous opening braces from stack
-    then check opening and closing belong to same type
-
+    /**
+     * Method for checking if brackets are balanced in a string.
+     *
+     * Iterates through the string for each character.
+     * When a closing bracket is encountered, pops the previous opening bracket from the stack
+     * and checks if the opening and closing brackets match.
+     *
+     * @param line the input string containing brackets
+     * @return true if the brackets are balanced, false otherwise
      */
     static boolean checkBalancedWithStack(String line) {
-        List<String> inputs = Stream.of(line.trim().split("")).collect(Collectors.toList());
-        List<String> openingList = List.of("(", "{", "[");
-        List<String> closingList = List.of(")", "}", "]");
 
-        // map for getting closing parenthesis using opening parenthesis
-        Map<String, String> openingClosing = new HashMap<>();
-        openingClosing.put("(", ")");
-        openingClosing.put("{", "}");
-        openingClosing.put("[", "]");
 
-        Stack<String> openingValueStack = new Stack<>(inputs.size());
-        for (String ch : inputs) {
-            // if letter is opening ([{ pushed to stack
-            if (openingList.contains(ch)) {
+
+
+        Stack<Character> openingValueStack = new Stack<>(line.length());
+        for (char ch : line.toCharArray()) {
+            // If the character is an opening bracket '([{', push it to the stack
+            if (OPENING_SET.contains(ch)) {
                 openingValueStack.push(ch);
             }
-            //if letter is closing ")]}" get previous opening parenthesis from stack
-            // and get the matching closing parenthesis from map check it is correct
-            else if (closingList.contains(ch)) {
-                if (!openingValueStack.isEmpty()) {
-                    String Closing = openingClosing.get(openingValueStack.pop());
-                    if (!ch.equals(Closing)) {
-                        return false;
-                    }
-                } else  return false;
+            // If the character is a closing bracket ')]}', get the previous opening bracket from the stack
+            // and check if it matches the expected closing bracket from the map
+            else if (CLOSING_SET.contains(ch)) {
+               if(openingValueStack.isEmpty()|| !OPENING_CLOSING.get(openingValueStack.pop()).equals(ch)) return false;
             }
         }
-        /* loop for processing string completed and all the values in
-        openingvaluestack is processedthen the string parenthesis are balanced  */
+        /* After processing the entire string, if all values in the openingValueStack have been processed,
+        then the string's brackets are balanced. */
         return openingValueStack.isEmpty();
     }
+    
 
-
+    /**
+     * Custom stack implementation.
+     *
+     * @param <E> the type of elements in the stack
+     */
     static class Stack<E> {
         E[] arr;
         int top = -1;
@@ -89,23 +91,40 @@ public class CheckOpeningClosingBracketsBalanced {
             arr = (E[]) new Object[capacity];
         }
 
+        /**
+         * Returns the size of the stack array.
+         *
+         * @return the capacity of the stack
+         */
         int size() {
             return arr.length;
         }
 
+        /**
+         * Pushes an element onto the stack.
+         *
+         * @param str the element to push
+         */
         void push(E str) {
             arr[++top] = str;
         }
 
+        /**
+         * Pops an element from the stack.
+         *
+         * @return the popped element
+         */
         E pop() {
             return arr[top--];
         }
 
+        /**
+         * Checks if the stack is empty.
+         *
+         * @return true if the stack is empty, false otherwise
+         */
         boolean isEmpty() {
             return top == -1;
         }
     }
 }
-
-
-
