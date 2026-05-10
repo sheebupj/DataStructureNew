b5f3b9de3aeb2f44b84eb8714a7331a22f1b2b22
Sheebu P J <Sheebupj@gmail.com>
2026-05-10 17:06:44 +0530

    error message modified

--- src/main/java/com/paremal/sheebu/algorithms/DigitsToWords.java
+++ src/main/java/com/paremal/sheebu/algorithms/DigitsToWords.java
@@ -90,7 +90,7 @@ public class DigitsToWords {
      */
     public static String digitToWordIndian(String digits) {
         //String digits = digit + "";
-        if(!digits.matches("\\d+")) return "invalid number format";
+        if(!digits.matches("\\d+")) return "invalid input";
         if (digits.length() > 12) return "maximum allowed digits is 12 and current input is " + digits.length();
         List<String> list = getSplitDigitsIndian(reverseWithStringBuilder(digits));
         List<String> suffixes = getSuffixesListIndian();
@@ -123,7 +123,7 @@ public class DigitsToWords {
      */
     public static String digitToWordMillionBillionFormat(String digits) {
         //String digits = digit + "";
-        if(!digits.matches("\\d+")) return "invalid number format";
+        if(!digits.matches("\\d+")) return "invalid input";
         if (digits.length() > 21)
             return "max digits allowed is 21 and  current input contains " + digits.length() + " digits";
         List<String> list = getSplitDigitsMillionBillionFormat(reverseWithStringBuilder(digits));
